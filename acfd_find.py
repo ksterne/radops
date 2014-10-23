c2c21f9368656d678ab1213697f24cfb49d6ade2
Kevin Sterne <kevintyler@vt.edu>
2014-10-23 10:14:12 -0400

    Fix indentation.
    Doh!

--- acfd_find.py
+++ acfd_find.py
@@ -37,7 +37,7 @@ def acfd_find_loop(sDate,radar):
 	#for key in myData.prm.__dict__.keys():
 	#	print 'myData.rawacf.'+key
 
-		print "Searching through files for missing acfd arrays..."
+	print "Searching through files for missing acfd arrays..."
 	while (myData != None):
 		try: array=myData.rawacf.acfd[0]
 		except IndexError:
